944b75169905f4ebdaec57d40ce1a863491fcc94
Niroj <30294440+sthajarins@users.noreply.github.com>
2025-08-21 22:09:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,6 @@ def table_data():
     return df.head(100).to_json(orient="records")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render sets $PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
